27ca6d859a96f010bb31c577523c44bd943a7f40
SR <savinien.rossi@carbon-it.com>
2025-07-02 17:19:38 +0200

    tests: add basic tests

--- src/test/kotlin/CalculatorTest.kt
+++ src/test/kotlin/CalculatorTest.kt
@@ -13,4 +13,25 @@ class CalculatorTest {
 
         assertEquals(0, result)
     }
+
+    @Test
+    fun `add returns value for string containing only one value`() {
+        val calculator = StringCalculator()
+        val input = "1"
+
+        val result = calculator.add(input)
+
+        assertEquals(1, result)
+    }
+
+    @Test
+    fun `add returns sum of two values for string containing two values separated by comma`() {
+        val calculator = StringCalculator()
+        val input = "1,2"
+
+        val result = calculator.add(input)
+
+        assertEquals(3, result)
+    }
+
 }
